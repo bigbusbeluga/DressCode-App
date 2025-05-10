9cfa38972e11a4078030ce053970fa7587fa7a5c
Carlo Panambo <panambocarlomario@gmail.com>
2025-05-10 20:45:14 +0800

    Added login and signup templates

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login')
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base.html')
+
+def signup_view(request):
+    return render(request, 'signup.html')
+
+def login_view(request):
+    return render(request, 'login.html')
