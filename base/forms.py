834d544edf651190c6b048098c85db29271dd1b2
Lester Liby <ojenarlester@gmail.com>
2025-05-13 23:39:42 +0800

    Log In and Sign Up Fields

---
+++ base/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SingUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "w-full rounded border px-4 py-2"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({"class": "w-full rounded border px-4 py-2"})
+        self.fields['password2'].widget.attrs.update({"class": "w-full rounded border px-4 py-2"})
--- base/views.py
+++ base/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SingUpForm 
+from django.contrib.auth import login as auth_login
 
-# Create your views here.
 def home(request):
     return render(request, 'base/base.html')
-  
+
 def signup(request):
-    return render(request, 'base/signup.html')
+    if request.method == "POST":
+        form = SingUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = SingUpForm()
+    return render(request, 'base/signup.html', {'form': form})
 
 def login(request):
-    return render(request, 'base/login.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is incorrect')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'base/login.html', {'form': form})
+
+def mixmatch(request):
+    return render(request, 'base/mixmatch.html')
 
