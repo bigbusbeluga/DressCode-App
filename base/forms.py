12aa692674a62c450e92b21a61a3ec3e9bea1224
Carlo Panambo <panambocarlomario@gmail.com>
2025-05-15 15:47:21 +0800

    Upload frontend

--- base/forms.py
+++ base/forms.py
@@ -21,3 +21,14 @@ class addClothingForm(ModelForm):
     class Meta:
         model = Clothing
         fields = ['name', 'brand', 'description', 'isFavorite', 'image', 'category']
+        labels = {
+            'isFavorite': 'Add to Favorites?',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'w-full rounded border px-4 py-2'}),
+            'brand': forms.TextInput(attrs={'class': 'w-full rounded border px-4 py-2'}),
+            'description': forms.Textarea(attrs={'class': 'w-full rounded border px-4 py-2', 'rows': 2}),
+            'isFavorite': forms.CheckboxInput(attrs={'class': 'rounded border px-4 py-2'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'w-full text-sm text-slate-500 rounded border file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-gray-100'}),
+            'category': forms.Select(attrs={'class': 'w-full rounded border px-4 py-2'}),
+        }
